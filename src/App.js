797952d21f7a5fee0abfccd6de59da62d4d5da59
JadenFicklin <jadendficklin@gmail.com>
2022-09-27 18:47:07 -0600

    imported assets (images and logos)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Landing />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <>
+      <div className="landing-outer"></div>
+    </>
+  );
+}
+
+export default Landing;
