bb6c4ba18eac685aa6857c3e293e5365c5c8ad00
JadenFicklin <jadendficklin@gmail.com>
2022-09-29 13:15:23 -0600

    some fixes to image functionality

--- src/components/ProjectOne.js
+++ src/components/ProjectOne.js
@@ -4,6 +4,7 @@ import "../styles/ProjectOne.css";
 function ProjectOne() {
   return (
     <>
+      {/* part one */}
       <div className="project-one-outer">
         <div className="project-one-top">
           <div className="project-one-top-line"></div>
@@ -35,8 +36,86 @@ function ProjectOne() {
         </div>
         <div className="project-one-right">
           <div className="project-one-right-inner">
-            <div className="project-one-right-inner-image-one"></div>
-            <div className="project-one-right-inner-image-two"></div>
+            <div className="project-one-right-inner-image-one image"></div>
+            <div className="project-one-right-inner-image-two image"></div>
+            <div className="project-one-right-inner-image-three image"></div>
+            <div className="project-one-right-inner-image-four image"></div>
+          </div>
+        </div>
+      </div>
+
+      {/* part two */}
+      <div className="project-one-outer">
+        <div className="project-one-top">
+          <div className="project-one-top-line"></div>
+        </div>
+        <div className="project-one-left">
+          <div className="project-one-left-left">
+            <div className="project-info-top">
+              <div className="header">PROJECT</div>
+              <div className="sub-header">Eromanga Natural History Museum</div>
+            </div>
+            <div className="project-info-middle"></div>
+            <div className="project-info-bottom">
+              <div className="project-info-bottom-top">
+                <div className="header">CATEGORY</div>
+                <div className="mini-header">Arts and Community</div>
+              </div>
+              <div className="project-info-bottom-bottom">
+                <div className="project-info-bottom-bottom-top">
+                  <div className="header">IMAGES 1/4</div>
+                </div>
+                <div className="project-info-bottom-bottom-bottom">
+                  <div className="left-button">left</div>
+                  <div className="right-button">right</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project-one-left-right"></div>
+        </div>
+        <div className="project-one-right">
+          <div className="project-one-right-inner">
+            {/* <div className="project-one-right-inner-image-one"></div> */}
+            {/* <div className="project-one-right-inner-image-two"></div> */}
+          </div>
+        </div>
+      </div>
+
+      {/* part three */}
+      <div className="project-one-outer">
+        <div className="project-one-top">
+          <div className="project-one-top-line"></div>
+        </div>
+        <div className="project-one-left">
+          <div className="project-one-left-left">
+            <div className="project-info-top">
+              <div className="header">PROJECT</div>
+              <div className="sub-header">Eromanga Natural History Museum</div>
+            </div>
+            <div className="project-info-middle"></div>
+            <div className="project-info-bottom">
+              <div className="project-info-bottom-top">
+                <div className="header">CATEGORY</div>
+                <div className="mini-header">Arts and Community</div>
+              </div>
+              <div className="project-info-bottom-bottom">
+                <div className="project-info-bottom-bottom-top">
+                  <div className="header">IMAGES 1/4</div>
+                </div>
+                <div className="project-info-bottom-bottom-bottom">
+                  <div className="left-button">left</div>
+                  <div className="right-button">right</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project-one-left-right"></div>
+        </div>
+        <div className="project-one-right">
+          <div className="project-one-right-inner">
+            {/* <div className="project-one-right-inner-image-one"></div> */}
+            {/* <div className="project-one-right-inner-image-two"></div> */}
           </div>
         </div>
       </div>
