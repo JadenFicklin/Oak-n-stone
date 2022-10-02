70795090e0ad78ec1685f5732d15f92bcce713e3
JadenFicklin <jadendficklin@gmail.com>
2022-10-01 19:42:52 -0600

    tied scroll position to wheel progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "../styles/Projects.css";
+import useScrollPosition from "../hooks/useScrollPosition";
 
 function Projects() {
+  const scrollPosition = useScrollPosition();
+  console.log(scrollPosition);
+
+  const scrollNumber = scrollPosition;
+
+  const pictureWheelStyling = {
+    backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='%23CFCFB0FF' stroke-width='4' stroke-dasharray='${scrollNumber}%2c 700' stroke-dashoffset='86' stroke-linecap='butt'/%3e%3c/svg%3e")`,
+  };
+
   return (
     <>
       <div className="projects-outer">
         <div className="project-left">
           <div className="project-wheel-outer">
-            <div className="project-wheel-inner"></div>
+            <div
+              className="project-wheel-inner"
+              style={pictureWheelStyling}
+            ></div>
           </div>
         </div>
         <div className="project-right">
