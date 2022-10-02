a722faeed55c561038bc65517519ea5a3dda41f2
JadenFicklin <jadendficklin@gmail.com>
2022-10-01 19:23:28 -0600

    boxing for projects tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,22 @@ import "../styles/Projects.css";
 function Projects() {
   return (
     <>
-      <div className="projects-outer"></div>
+      <div className="projects-outer">
+        <div className="project-left">
+          <div className="project-wheel-outer">
+            <div className="project-wheel-inner"></div>
+          </div>
+        </div>
+        <div className="project-right">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div>
     </>
   );
 }
