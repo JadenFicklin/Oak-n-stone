906a4eb7fea0f4b7f01fa15b99a5af269169769d
JadenFicklin <jadendficklin@gmail.com>
2022-10-06 13:50:47 -0600

    added in intersection points for wheel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,6 +14,17 @@ function Projects() {
           <div className="percent">
             <svg viewBox="-50 -50 300 300">
               <circle
+                className="background-cirlce"
+                cx="90"
+                cy="90"
+                r="90"
+                stroke-dashoffset={0}
+              />
+            </svg>
+          </div>
+          <div className="percent">
+            <svg viewBox="-50 -50 300 300">
+              <circle
                 cx="90"
                 cy="90"
                 r="90"
@@ -23,6 +34,12 @@ function Projects() {
           </div>
         </div>
       </div>
+      <div className="circle-one bubble"></div>
+      <div className="circle-two bubble"></div>
+      <div className="circle-three bubble"></div>
+      <div className="circle-four bubble"></div>
+      <div className="circle-five bubble"></div>
+      <div className="circle-six bubble"></div>
       <div className="project-right">
         <div className="row"></div>
         <div className="row"></div>
