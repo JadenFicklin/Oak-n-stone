7216b9466726ab73639ec70f77b248c29bd9f2f4
JadenFicklin <jadendficklin@gmail.com>
2022-10-19 15:17:34 -0600

    added contact section functionality and dotenv

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/Contacts.css";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const [topLine, setTopLine] = useState(false);
@@ -9,6 +10,27 @@ function Contact() {
   const [lineTwo, setLineTwo] = useState(false);
   const [lineThree, setLineThree] = useState(false);
   const [lineFour, setLineFour] = useState(false);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e) => {
+    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+    setInputValue(formattedPhoneNumber);
+  };
+
+  function formatPhoneNumber(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, "");
+    const phoneNumberLength = phoneNumber.length;
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+      3,
+      6
+    )}-${phoneNumber.slice(6, 10)}`;
+  }
 
   setTimeout(() => {
     setTopLine(true);
@@ -29,9 +51,36 @@ function Contact() {
     setLineFour(true);
   }, 1200);
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `
+        ${process.env.REACT_APP_API_KEY_ONE},
+        ${process.env.REACT_APP_API_KEY_TWO},
+        form.current,
+        ${process.env.REACT_APP_API_KEY_THREE}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          console.log("message sent");
+          setInputValue("");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact-outer">
       <div className="contact-inner">
+        {/* lines */}
         <div
           className={
             topLine
@@ -47,7 +96,46 @@ function Contact() {
           }
         ></div>
         <div className="contact-inner-box">
+          {/* left box */}
           <div className="contact-inner-box-left">
+            {/* inner left box */}
+            <div className="contact-inner-box-left-inner">
+              {/* <div className="contact-us-header">CONTACT US</div>
+
+              <container>
+                <div className="contact-name-text">Name:</div>
+                <input className="contact-us-name-input"></input>
+              </container>
+              <container>
+                <div className="contact-email-text">Email:</div>
+                <input className="contact-us-email-input"></input>
+              </container>
+
+              <container>
+                <div className="contact-message-text">Message:</div>
+                <textarea className="contact-us-message-input"></textarea>
+              </container>
+
+              <button className="send-email">Send Email</button> */}
+              <form ref={form} onSubmit={sendEmail}>
+                <label>First Name</label>
+                <input type="text" name="first_name" />
+                <label>Last Name</label>
+                <input type="text" name="last_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Phone Number</label>
+                <input
+                  type="tel"
+                  name="user_phone"
+                  onChange={(e) => handleInput(e)}
+                  value={inputValue}
+                />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+              </form>
+            </div>
             <div
               className={
                 lineOne
@@ -63,8 +151,13 @@ function Contact() {
               }
             ></div>
           </div>
+
+          {/* right box */}
           <div className="contact-inner-box-right">
+            {/* map box */}
             <div className="map-box-outer"></div>
+
+            {/* info box */}
             <div className="contact-info-box-outer">
               <div
                 className={
