c705d1f6b1dbd8fcccaddd21d980d066778fec76
JadenFicklin <jadendficklin@gmail.com>
2022-10-26 22:39:22 -0600

    some fixes and tab icon/text

--- src/App.js
+++ src/App.js
@@ -9,133 +9,23 @@ import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import ErrorPage from "./components/ErrorPage";
+import { Helmet } from "react-helmet";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [projects, setProjects] = useState(false);
-  // const [about, setAbout] = useState(false);
-  // const [contact, setContact] = useState(false);
-
-  // const [nav, setNav] = useState(false);
-
-  // const [homePage, setHomePage] = useState(true);
-  // const [projectsPage, setProjectsPage] = useState(false);
-  // const [aboutPage, setAboutPage] = useState(false);
-  // const [contactPage, setContactPage] = useState(false);
-
-  // setTimeout(() => {
-  //   setNav(true);
-  // }, 1600);
-
-  // const changeScroll = (position) => {
-  //   window.scrollTo(0, position);
-  // };
-
-  // const handleHomeClick = () => {
-  //   setHomePage(true);
-  //   setProjectsPage(false);
-  //   setAboutPage(false);
-  //   setContactPage(false);
-  //   changeScroll(0);
-  // };
-  // const handleProjectClick = () => {
-  //   setHomePage(false);
-  //   setProjectsPage(true);
-  //   setAboutPage(false);
-  //   setContactPage(false);
-  //   changeScroll(0);
-  // };
-  // const handleAboutClick = () => {
-  //   setHomePage(false);
-  //   setProjectsPage(false);
-  //   setAboutPage(true);
-  //   setContactPage(false);
-  //   changeScroll(0);
-  // };
-  // const handleContactClick = () => {
-  //   setHomePage(false);
-  //   setProjectsPage(false);
-  //   setAboutPage(false);
-  //   setContactPage(true);
-  //   changeScroll(0);
-  // };
-
   return (
     <>
-      {/* <div className={nav ? "nav-outer" : "nav-outer transparent"}>
-        <nav className="nav-inner">
-          <div className="nav-logo">
-            <img
-              src={require("../src/assets/logos/logo.JPG")}
-              alt="logo"
-              className="nav-logo-inner"
-              onClick={handleHomeClick}
-            />
-          </div>
-          <div
-            className="nav-sub"
-            onMouseEnter={() => setProjects(true)}
-            onMouseLeave={() => setProjects(false)}
-            onClick={handleProjectClick}
-          >
-            PROJECTS
-            <div
-              className={
-                projects ? "nav-line-hover-true" : "nav-line-hover-false"
-              }
-            ></div>
-          </div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Oak and Stone</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta
+          name="description"
+          content="utah construction remodel cabinets oak and stone"
+        />
+      </Helmet>
 
-          <div
-            className="nav-sub"
-            onMouseEnter={() => setAbout(true)}
-            onMouseLeave={() => setAbout(false)}
-            onClick={handleAboutClick}
-          >
-            ABOUT
-            <div
-              className={about ? "nav-line-hover-true" : "nav-line-hover-false"}
-            ></div>
-          </div>
-          <div
-            className="nav-sub"
-            onMouseEnter={() => setContact(true)}
-            onMouseLeave={() => setContact(false)}
-            onClick={handleContactClick}
-          >
-            CONTACT
-            <div
-              className={
-                contact ? "nav-line-hover-true" : "nav-line-hover-false"
-              }
-            ></div>
-          </div>
-        </nav>
-      </div>
-
-      {homePage && (
-        <>
-          <Landing />
-          <SelectedWorks />
-          <ProjectOne />
-        </>
-      )}
-      {projectsPage && (
-        <>
-          <Projects />
-        </>
-      )}
-      {aboutPage && (
-        <>
-          <About />
-        </>
-      )}
-      {contactPage && (
-        <>
-          <Contact />
-        </>
-      )} */}
       <Router>
         <Routes>
           <Route
